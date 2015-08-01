71a634919ba04f9ca207a168c7aed23dda183cb9
Alexander <alex.steve.elias@gmail.com>
2015-08-01 12:05:54 -0700

    Minor updates

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 
 #[macro_use] extern crate nickel;
-
-use nickel::{Nickel, HttpRouter, StaticFilesHandler, FaviconHandler, MediaType};
+use nickel::{Nickel, HttpRouter, StaticFilesHandler, FaviconHandler};
 use std::collections::HashMap;
 
+// Needed For Post
+extern crate hyper;
+use std::io::Read;
+use hyper::Client;
+use hyper::header::Connection;
+// Needed For Post
 
 fn main() {
 
     //Implement A Server
     let mut server = Nickel::new();
 
-
-
     //Route Using Mustache Templateing Agent
     server.get("/", middleware! { |req, mut res|
 
         let mut data = HashMap::<&str, &str>::new();
-        data.insert("tab-title","Verge");
+        data.insert("tab-title","Verge | Web Development, Design, and SEO Company In Tucson");
         data.insert("title","<span style=\"margin-left: 28.9px; position: absolute; margin-top: 17px;vertical-align: middle;\">V</span><span style=\"font-size: 145%; color: #C36159;vertical-align: middle;\">&#9678</span><span style=\"margin-left: -7px; vertical-align: middle;\">erge</span></h1>");
         data.insert("sub-title", "Web Development");
         data.insert("hint", "<div id=\"hint\"><span id=\"message\">Use Arrow Keys</span><div>&#x276f;</div></div>");
@@ -26,15 +29,38 @@ fn main() {
         return res.render("views/index.html", &data);
     });
 
-    server.get("/contact", middleware! { |req, res|
-
-        let mut data = HashMap::<&str, &str>::new();
-        data.insert("tab-title","Contact");
+    server.get("/contact", middleware! { |req, mut res|
+        let mut data = HashMap::new();
+        data.insert("tab-title","Contact Us | Web Development, Design, and SEO Company In Tucson");
         data.insert("title","Contact");
 
-        return res.render("views/contact.html", &data);
+        let mut s = String::new();
+        req.origin.read_to_string(&mut s);
+
+        return res.render("views/contact.html", &data)
     });
 
+    server.post("/submit", middleware!{ |req, res|
+        let mut s = String::new();
+        req.origin.read_to_string(&mut s);
+        println!("First Name: {}", s);
+
+        let the = s.to_string();
+
+        let mut data = HashMap::new();
+        data.insert("tab-title","Submit | Verge Web Development, Design, and SEO Company In Tucson");
+        data.insert("title","Submit");
+        data.insert("confirmation", &the);
+        return res.render("views/submit.html", &data)
+    });
+
+    server.get("/about", middleware!{ |req, res|
+        let mut data = HashMap::new();
+        data.insert("tab-title","About");
+        data.insert("title","About");
+
+        return res.render("views/about.html", &data);
+    });
 
     //Serve A Static Directory
     server.utilize(StaticFilesHandler::new("views/public/"));
--- views/public/js/dynamicTiles.js
+++ views/public/js/dynamicTiles.js
@@ -34,138 +34,139 @@ function setDefaults() {
       var back = document.getElementsByClassName('back');
       var front = document.getElementsByClassName('front');
 
+      if (typeof(tilesGroup) != 'undefind' && tilesGroup != null) {
+            
+            //This function sets an element to the remaining parent height
+            function setToRemaingParentHeight(elm){
+                  //Set the element height to the innerHeight minus the all previous elements
+                  var newElmHeight = elm.parentElement.offsetHeight - elm.offsetTop;
 
-      //This function sets an element to the remaining parent height
-      function setToRemaingParentHeight(elm){
-            //Set the element height to the innerHeight minus the all previous elements
-            var newElmHeight = elm.parentElement.offsetHeight - elm.offsetTop;
-
-            if(!tilesGroup.nextElementSibling || tilesGroup.nextElementSibling.offsetHeight <= 0){
-                  //The 22px is to account for a padding error
-                  return newElmHeight + 22;
-            }else if(tilesGroup.nextElementSibling.offsetHeight > 0){
-                  var nextElementHeight = tilesGroup.nextElementSibling.offsetHeight;
-                  return newElmHeight - nextElementHeight;
+                  if(!tilesGroup.nextElementSibling || tilesGroup.nextElementSibling.offsetHeight <= 0){
+                        //The 22px is to account for a padding error
+                        return newElmHeight + 22;
+                  }else if(tilesGroup.nextElementSibling.offsetHeight > 0){
+                        var nextElementHeight = tilesGroup.nextElementSibling.offsetHeight;
+                        return newElmHeight - nextElementHeight;
+                  }
             }
-      }
-      var tilesGroupHeight = setToRemaingParentHeight(tilesGroup);
-      var tilesGroupHeight = tilesGroupHeight;
+            var tilesGroupHeight = setToRemaingParentHeight(tilesGroup);
+            var tilesGroupHeight = tilesGroupHeight;
 
-      tilesGroup.style.height = tilesGroupHeight + 'px';
-      tilesGroup.style.textAlign = 'center';
-      tilesGroup.style.msPerspective = '1000px'
-      tilesGroup.style.mozPerspective = '1000px'
-      tilesGroup.style.webkitPerspective = '1000px'
-      tilesGroup.style.perspective = '1000px'
+            tilesGroup.style.height = tilesGroupHeight + 'px';
+            tilesGroup.style.textAlign = 'center';
+            tilesGroup.style.msPerspective = '1000px'
+            tilesGroup.style.mozPerspective = '1000px'
+            tilesGroup.style.webkitPerspective = '1000px'
+            tilesGroup.style.perspective = '1000px'
 
-      if (window.innerWidth <= 770) {
-            var tilesGroupHeight = tilesGroupHeight - (tilesGroupHeight * 0.10);
-            tilesGroup.style.marginBottom = '2%';
-            tilesGroup.style.marginTop = '2%';
-      }
-
-
-      var desktopWidth = (window.innerWidth / tile.length) + (3 * -10) - ((window.innerWidth / tile.length)* 0.05) + 'px';
-      var desktopHeight = tilesGroupHeight + 'px';
-      var desktopMinWidth = '200px';
-
-      var mediumWidth = (tilesGroup.offsetWidth / tile.length) + 'px';
-      var mediumHeight = (tilesGroupHeight / 2) + 'px';
-      var mediumMinWidth = '200px';
-
-      var smallWidth = '100%';
-      var smallHeight = (tilesGroupHeight / tile.length) + 'px';
-      var smallMinWidth = '100px';
-
-      for (var i = 0; i < tile.length; i++) {
-            //Constants
-            tileWrap[i].style.display = 'inline-block';
-
-            tile[i].style.mozTransition = '1s';
-            tile[i].style.webkitTransition = '1s';
-            tile[i].style.transition = '1s';
-            tile[i].style.mozTransformStyle = 'preserve-3d';
-            tile[i].style.webkitTransformStyle = 'preserve-3d';
-            tile[i].style.transformStyle = 'preserve-3d';
-
-            front[i].style.position = 'absolute';
-            front[i].style.webkitBackfaceVisibility = 'hidden';
-            front[i].style.backfaceVisibility = 'hidden';
-            front[i].style.zIndex = '2';
-            front[i].style.mozTransform = 'rotateY(0deg)';
-            front[i].style.webkitTransform = 'rotateY(0deg)';
-            front[i].style.transform = 'rotateY(0deg)';
-            front[i].style.display = 'table';
-            front[i].children[0].style.display = 'table-cell';
-            front[i].children[0].style.verticalAlign = 'middle';
-
-
-            back[i].style.position = 'absolute';
-            back[i].style.webkitBackfaceVisibility = 'hidden';
-            back[i].style.backfaceVisibility = 'hidden';
-            back[i].style.backgroundColor = '#333841';
-            back[i].style.mozTransform = 'rotateY(180deg)';
-            back[i].style.webkitTransform = 'rotateY(180deg)';
-            back[i].style.transform = 'rotateY(180deg)';
-            back[i].style.display = 'table';
-            back[i].children[0].style.display = 'table-cell';
-            back[i].children[0].style.verticalAlign = 'middle';
-            //Constants
-
-            if (window.innerWidth < 414) {
-                  tileWrap[i].style.width = '80%';
-                  tileWrap[i].style.height = smallHeight;
-                  tileWrap[i].style.minWidth = smallMinWidth;
-                  tileWrap[i].style.marginLeft = 'atuo';
-                  tileWrap[i].style.marginRight = 'atuo';
-
-                  tile[i].style.width = smallWidth;
-                  tile[i].style.height = smallHeight;
-                  tile[i].style.minWidth = smallMinWidth;
-
-                  front[i].style.width = smallWidth;
-                  front[i].style.height = smallHeight;
-                  front[i].style.minWidth = smallMinWidth;
-
-                  back[i].style.width = smallWidth;
-                  back[i].style.height = smallHeight;
-                  back[i].style.minWidth = smallMinWidth;
+            if (window.innerWidth <= 770) {
+                  var tilesGroupHeight = tilesGroupHeight - (tilesGroupHeight * 0.10);
+                  tilesGroup.style.marginBottom = '2%';
+                  tilesGroup.style.marginTop = '2%';
             }
 
-            if (window.innerWidth <= 770 && window.innerWidth >= 410){
-                  tileWrap[i].style.width = mediumWidth;
-                  tileWrap[i].style.height = mediumHeight;
-                  tileWrap[i].style.minWidth = mediumMinWidth;
 
-                  tile[i].style.width = mediumWidth;
-                  tile[i].style.height = mediumHeight;
-                  tile[i].style.minWidth = mediumMinWidth;
+            var desktopWidth = (window.innerWidth / tile.length) + (3 * -10) - ((window.innerWidth / tile.length)* 0.05) + 'px';
+            var desktopHeight = tilesGroupHeight + 'px';
+            var desktopMinWidth = '200px';
 
-                  front[i].style.width = mediumWidth;
-                  front[i].style.height = mediumHeight;
-                  front[i].style.minWidth = mediumMinWidth;
+            var mediumWidth = (tilesGroup.offsetWidth / tile.length) + 'px';
+            var mediumHeight = (tilesGroupHeight / 2) + 'px';
+            var mediumMinWidth = '200px';
 
-                  back[i].style.width = mediumWidth;
-                  back[i].style.height = mediumHeight;
-                  back[i].style.minWidth = mediumMinWidth;
-            }
+            var smallWidth = '100%';
+            var smallHeight = (tilesGroupHeight / tile.length) + 'px';
+            var smallMinWidth = '100px';
 
-            if (window.innerWidth > 770) {
-                  tileWrap[i].style.width = desktopWidth;
-                  tileWrap[i].style.height = desktopHeight;
-                  tileWrap[i].style.minWidth = desktopMinWidth;
+            for (var i = 0; i < tile.length; i++) {
+                  //Constants
+                  tileWrap[i].style.display = 'inline-block';
 
-                  tile[i].style.width = desktopWidth;
-                  tile[i].style.height = desktopHeight;
-                  tile[i].style.minWidth = mediumMinWidth;
+                  tile[i].style.mozTransition = '1s';
+                  tile[i].style.webkitTransition = '1s';
+                  tile[i].style.transition = '1s';
+                  tile[i].style.mozTransformStyle = 'preserve-3d';
+                  tile[i].style.webkitTransformStyle = 'preserve-3d';
+                  tile[i].style.transformStyle = 'preserve-3d';
 
-                  front[i].style.width = desktopWidth;
-                  front[i].style.height = desktopHeight;
-                  front[i].style.minWidth = mediumMinWidth;
+                  front[i].style.position = 'absolute';
+                  front[i].style.webkitBackfaceVisibility = 'hidden';
+                  front[i].style.backfaceVisibility = 'hidden';
+                  front[i].style.zIndex = '2';
+                  front[i].style.mozTransform = 'rotateY(0deg)';
+                  front[i].style.webkitTransform = 'rotateY(0deg)';
+                  front[i].style.transform = 'rotateY(0deg)';
+                  front[i].style.display = 'table';
+                  front[i].children[0].style.display = 'table-cell';
+                  front[i].children[0].style.verticalAlign = 'middle';
 
-                  back[i].style.width = desktopWidth;
-                  back[i].style.height = desktopHeight;
-                  back[i].style.minWidth = mediumMinWidth;
+                  back[i].style.position = 'absolute';
+                  back[i].style.webkitBackfaceVisibility = 'hidden';
+                  back[i].style.backfaceVisibility = 'hidden';
+                  back[i].style.backgroundColor = '#333841';
+                  back[i].style.mozTransform = 'rotateY(180deg)';
+                  back[i].style.webkitTransform = 'rotateY(180deg)';
+                  back[i].style.transform = 'rotateY(180deg)';
+                  back[i].style.display = 'table';
+                  back[i].children[0].style.display = 'table-cell';
+                  back[i].children[0].style.verticalAlign = 'middle';
+                  //Constants
+
+                  if (window.innerWidth < 414) {
+                        tileWrap[i].style.width = '80%';
+                        tileWrap[i].style.height = smallHeight;
+                        tileWrap[i].style.minWidth = smallMinWidth;
+                        tileWrap[i].style.marginLeft = 'atuo';
+                        tileWrap[i].style.marginRight = 'atuo';
+
+                        tile[i].style.width = smallWidth;
+                        tile[i].style.height = smallHeight;
+                        tile[i].style.minWidth = smallMinWidth;
+
+                        front[i].style.width = smallWidth;
+                        front[i].style.height = smallHeight;
+                        front[i].style.minWidth = smallMinWidth;
+
+                        back[i].style.width = smallWidth;
+                        back[i].style.height = smallHeight;
+                        back[i].style.minWidth = smallMinWidth;
+                  }
+
+                  if (window.innerWidth <= 770 && window.innerWidth >= 410){
+                        tileWrap[i].style.width = mediumWidth;
+                        tileWrap[i].style.height = mediumHeight;
+                        tileWrap[i].style.minWidth = mediumMinWidth;
+
+                        tile[i].style.width = mediumWidth;
+                        tile[i].style.height = mediumHeight;
+                        tile[i].style.minWidth = mediumMinWidth;
+
+                        front[i].style.width = mediumWidth;
+                        front[i].style.height = mediumHeight;
+                        front[i].style.minWidth = mediumMinWidth;
+
+                        back[i].style.width = mediumWidth;
+                        back[i].style.height = mediumHeight;
+                        back[i].style.minWidth = mediumMinWidth;
+                  }
+
+                  if (window.innerWidth > 770) {
+                        tileWrap[i].style.width = desktopWidth;
+                        tileWrap[i].style.height = desktopHeight;
+                        tileWrap[i].style.minWidth = desktopMinWidth;
+
+                        tile[i].style.width = desktopWidth;
+                        tile[i].style.height = desktopHeight;
+                        tile[i].style.minWidth = mediumMinWidth;
+
+                        front[i].style.width = desktopWidth;
+                        front[i].style.height = desktopHeight;
+                        front[i].style.minWidth = mediumMinWidth;
+
+                        back[i].style.width = desktopWidth;
+                        back[i].style.height = desktopHeight;
+                        back[i].style.minWidth = mediumMinWidth;
+                  }
             }
       }
 }
